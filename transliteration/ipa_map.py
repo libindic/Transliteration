782aca5e9106b1de2f5253d3fc290ecf978fd276
Pravendra Singh <hackpravj@gmail.com>
2014-04-17 04:32:18 +0530

    fixes broken translation to IPA format

---
+++ transliteration/ipa_map.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#
+# If you find any bugs or have any suggestions
+# email: hackpravj@gmail.com
+# URL: https://pravj.github.io
+
+__all__ = ["IPA_Symbol_MAP"]
+
+IPA_Symbol_MAP = {
+	"AO": "ɔ",
+	"AA": "ɑ",
+	"AH1": "ʌ",
+	"AH0": "ə",
+	"AX": "ə",
+	"IY": "i",
+	"UW": "u",
+	"EH": "ɛ",
+	"IH": "ɪ",
+	"UH": "ʊ",
+	"AE": "æ",
+	"EY": "eɪ",
+	"AY": "aɪ",
+	"OW": "oʊ",
+	"AW": "aʊ",
+	"OY": "ɔɪ",
+	"CH": "tʃ",
+	"JH": "dʒ",
+	"TH": "θ",
+	"DH": "ð",
+	"SH": "ʃ",
+	"ZH": "ʒ",
+	"HH": "h",
+	"EM": "m̩",
+	"EN": "n̩",
+	"NG": "ŋ",
+	"ENG": "ŋ̍",
+	"Y": "j",
+	"Q": "ʔ"
+}
